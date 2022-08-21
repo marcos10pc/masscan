eb95d4b1ec54a766f5db6e246a25e25704a65d21
mat <27899617+mat-1@users.noreply.github.com>
2022-08-20 22:00:46 -0500

    change minecraft hello

--- src/proto-minecraft.c
+++ src/proto-minecraft.c
@@ -7,14 +7,15 @@
 #define STATE_READ_DATA       4
 
 static const char minecraft_hello[] = {
-    0x15, // length
-    0x00, // packet ID (handshake)
-    0xff, 0xff, 0xff, 0xff, 0x0f, // protocol version number (-1 for ping) 
-    0x0b, 0x65, 0x78, 0x61, 0x6d, 0x70, 0x6c, 0x65, 0x2e, 0x63, 0x6f, 0x6d, // hostname (we just use 'example.com')
-    0xdd, 0x36, // port (stick with 25565)
-    0x01, // next state (1 for querying server status)
-    0x01, // length 
-    00 // packet ID (request status)
+    0x11, // length
+    0x00, // packet id
+    0xff, 0xff, 0xff, 0xff, 0x0f, // protocol version number (-1)
+    0x07, 0x6d, 0x61, 0x73, 0x73, 0x63, 0x61, 0x6e, /// hostname (masscan)
+    0x00 0x0, // port (0)
+    0x01, // next state (status)
+
+    0x01, // length
+    0x00 // packet id (request status)
 };
 
 static void minecraft_parse(const struct Banner1 *banner1,
